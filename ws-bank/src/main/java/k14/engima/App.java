b408ba0d65bf8df6d9b95194ba43ab92786f68cd
lukaskurnia <lukasjonathan99@gmail.com>
2019-11-07 15:04:14 +0700

    initial commit

---
+++ ws-bank/src/main/java/k14/engima/App.java
@@ -0,0 +1,13 @@
+package k14.engima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ws-bank/src/test/java/k14/engima/AppTest.java
@@ -0,0 +1,20 @@
+package k14.engima;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
