2f8b525dd4c8bb9e789cdee969b0a39f8a5fcca6
unknown <13516042@std.stei.itb.ac.id>
2019-11-21 23:13:32 +0700

    initialize model class

---
+++ src/main/java/k14/engima/src/components/nasabah.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package k14.engima.src.components;
+
+/**
+ * @author Seldi K Trihardja
+ *
+ */
+public class nasabah {
+	int nasabah_id;
+	String name;
+	String no_rek;
+	String virtual_acc;
+	int saldo;
+	String password;
+
+}
